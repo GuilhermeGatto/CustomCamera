4b4f63a5cd22bf85993af8a7cf96674c9f9e54bd
Guilherme Gatto <guuilhermegatto@hotmail.com>
2017-08-04 19:16:27 -0300

    outlet e actions criadas

--- CustomCamera/CustomCamera/ViewController.swift
+++ CustomCamera/CustomCamera/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var previewView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func capture(_ sender: Any) {
     }
-
+    
+    @IBAction func changeCamera(_ sender: Any) {
+    }
 
 }
 
